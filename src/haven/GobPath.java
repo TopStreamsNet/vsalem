dd0e236bb5cca0b19adcbb213ee769fdafaabeeb
Simon Donne <donnesimon@gmail.com>
2015-06-20 00:53:22 +0200

    GobPath crash fix

--- src/haven/GobPath.java
+++ src/haven/GobPath.java
@@ -60,7 +60,8 @@ public class GobPath extends Sprite
 	GOut.checkerr(gl);
     }
 
-    private synchronized Coord target(){
+    private Coord target(){
+        Moving move = move();
 	if(move != null){
 	    Class<? extends GAttrib> aClass = move.getClass();
 	    if(aClass == LinMove.class){
@@ -104,6 +105,10 @@ public class GobPath extends Sprite
 	return true;
     }
 
+    public synchronized Moving move() {
+	return move;
+    }
+    
     public synchronized void move(Moving m) {
 	move = m;
     }
