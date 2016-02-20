f6215d13e29febc6d93d0e09af55569400cd4d87
Simon Donne <donnesimon@gmail.com>
2016-02-20 15:12:35 +0100

    The player icon is now always below all other icons on the minimap The party cross is still on top of everything. Closes #26

--- src/haven/minimap/Marker.java
+++ src/haven/minimap/Marker.java
@@ -3,114 +3,122 @@ package haven.minimap;
 import haven.*;
 import java.awt.Color;
 
-public class Marker implements Comparable{
+public class Marker implements Comparable {
+
     public final String name;
     public final Gob gob;
     public final MarkerTemplate template;
+    public int order;
 
     public int compareTo(Object that) {
-        return this.template.order - ((Marker) that).template.order;
+        return this.order - ((Marker) that).order;
     }
 
-    private boolean override=false;
+    private boolean override = false;
     private String override_name;
     private Color override_color;
-    
-    public static enum Shape{
-	CIRCLE(10),
-	TRIANGLE(13),
-	TRIANGLED(13),
-	DIAMOND(13),
-	SQUARE(9),
-	PENTAGON(13);
-	
-	public Tex tex;
-	public int sz;
 
-	Shape(int sz){
-	    this.sz = sz;
-	}
-	
-	public static Shape get(String val){
-	    if(val.equals("circle")){
-		return CIRCLE;
-	    } else if(val.equals("up")){
-		return TRIANGLE;
-	    } else if(val.equals("down")){
-		return TRIANGLED;
-	    } else if(val.equals("diamond")){
-		return DIAMOND;
-	    } else if(val.equals("square")){
-		return SQUARE;
-	    } else if(val.equals("pentagon")){
-		return PENTAGON;
-	    }
-	    return CIRCLE;
-	}
-	
+    public static enum Shape {
+
+        CIRCLE(10),
+        TRIANGLE(13),
+        TRIANGLED(13),
+        DIAMOND(13),
+        SQUARE(9),
+        PENTAGON(13);
+
+        public Tex tex;
+        public int sz;
+
+        Shape(int sz) {
+            this.sz = sz;
+        }
+
+        public static Shape get(String val) {
+            if (val.equals("circle")) {
+                return CIRCLE;
+            } else if (val.equals("up")) {
+                return TRIANGLE;
+            } else if (val.equals("down")) {
+                return TRIANGLED;
+            } else if (val.equals("diamond")) {
+                return DIAMOND;
+            } else if (val.equals("square")) {
+                return SQUARE;
+            } else if (val.equals("pentagon")) {
+                return PENTAGON;
+            }
+            return CIRCLE;
+        }
+
     }
-    
+
     public Marker(String name, Gob gob, MarkerTemplate template) {
         this.name = name;
         this.gob = gob;
         this.template = template;
-        if(template.shape.tex == null){
+        this.order = template.order;
+        if (template.shape.tex == null) {
             template.shape.tex = Utils.generateMarkerTex(template.shape);
         }
     }
-    
-    public boolean hit(Coord c) {
-        Coord3f ptc3f = gob.getc();
-        if (ptc3f == null)
-            return false;
-        Coord p = new Coord((int)ptc3f.x, (int)ptc3f.y);
-	int radius = 4;
-	return (c.x - p.x) * (c.x - p.x) + (c.y - p.y) * (c.y - p.y) < radius * radius * MCache.tilesz.x * MCache.tilesz.y;
+
+    public void setOrder(int order) {
+        this.order = order;
     }
 
-    public void override(String name, Color c)
-    {
+    public boolean hit(Coord c) {
+        Coord3f ptc3f = gob.getc();
+        if (ptc3f == null) {
+            return false;
+        }
+        Coord p = new Coord((int) ptc3f.x, (int) ptc3f.y);
+        int radius = 4;
+        return (c.x - p.x) * (c.x - p.x) + (c.y - p.y) * (c.y - p.y) < radius * radius * MCache.tilesz.x * MCache.tilesz.y;
+    }
+
+    public void override(String name, Color c) {
         this.override = true;
         this.override_name = name;
         this.override_color = c;
     }
-    
-    public String getTooltip()
-    {
-        if(override)
-        {
+
+    public String getTooltip() {
+        if (override) {
             return override_name;
-        }
-        else{
+        } else {
             return this.template.tooltip;
         }
     }
-    
+
     public void draw(GOut g, Coord c) {
-        draw(g,c,1.0);
+        draw(g, c, 1.0);
     }
-    
+
     public void draw(GOut g, Coord c, double scale) {
         Coord3f ptc3f = gob.getc();
-        if (ptc3f == null)
+        if (ptc3f == null) {
             return;
-        Coord ptc = new Coord((int)ptc3f.x, (int)ptc3f.y);
+        }
+        Coord ptc = new Coord((int) ptc3f.x, (int) ptc3f.y);
         ptc = ptc.div(MCache.tilesz).add(c);
-        if(Config.radar_icons){
+        if (Config.radar_icons) {
             try {
-        	GobIcon icon = gob.getattr(GobIcon.class);
-        	if(icon != null && template.showicon) {
-        	    Tex tex = icon.tex();
-        	    g.image(tex, ptc.sub(tex.sz().div(2)), tex.sz().div(scale));
-        	    return;
-        	}
-            } catch(Loading l) {}
+                GobIcon icon = gob.getattr(GobIcon.class);
+                if (icon != null && template.showicon) {
+                    Tex tex = icon.tex();
+                    g.image(tex, ptc.sub(tex.sz().div(2)), tex.sz().div(scale));
+                    return;
+                }
+            } catch (Loading l) {
+            }
         }
         g.chcolor(template.color);
-        if(override)
+        if (override) {
             g.chcolor(override_color);
+        }
         Tex tex = template.shape.tex;
-	g.image(tex, ptc.sub(tex.sz().div(2)), tex.sz().div(scale));
+        g.image(tex, ptc.sub(tex.sz().div(2)), tex.sz().div(scale));
         g.chcolor();
     }
 }
--- src/haven/minimap/MarkerFactory.java
+++ src/haven/minimap/MarkerFactory.java
@@ -7,6 +7,7 @@ import haven.*;
 import haven.minimap.Marker.Shape;
 
 public class MarkerFactory {
+
     private final Set<String> unknownNameCache;
     private final Map<String, MarkerTemplate> templateCache;
     private final Map<String, ConfigMarker> matches;
@@ -15,8 +16,8 @@ public class MarkerFactory {
     public MarkerFactory() {
         this(new RadarConfig());
     }
-    
-    public MarkerFactory(RadarConfig rc){
+
+    public MarkerFactory(RadarConfig rc) {
         unknownNameCache = new HashSet<String>();
         templateCache = new HashMap<String, MarkerTemplate>();
         matches = new HashMap<String, ConfigMarker>();
@@ -26,33 +27,44 @@ public class MarkerFactory {
     }
 
     public Marker makeMarker(String resname, Gob gob) {
-	MarkerTemplate mt = findTemplate(resname);
-	if ((mt == null) && Config.radar_icons && (gob.getattr(GobIcon.class) != null)){
-	    mt = new MarkerTemplate(Color.WHITE, true, resname, true, Shape.CIRCLE,0,true);
-	    templateCache.put(resname, mt);
-	}
-	if(mt != null){
-	    return new Marker(resname, gob, mt);
-	}
-	return null;
+        MarkerTemplate mt = findTemplate(resname);
+        if ((mt == null) && Config.radar_icons && (gob.getattr(GobIcon.class) != null)) {
+            mt = new MarkerTemplate(Color.WHITE, true, resname, true, Shape.CIRCLE, 0, true);
+            templateCache.put(resname, mt);
+        }
+        if (mt != null) {
+            Marker m = new Marker(resname, gob, mt);
+            try {
+                if (gob == UI.instance.gui.map.player()) {
+                    m.setOrder(-100);
+                }
+            } catch (NullPointerException npe) {
+
+            }
+            return m;
+        }
+        return null;
     }
 
     private MarkerTemplate findTemplate(String resname) {
-        if (unknownNameCache.contains(resname))
+        if (unknownNameCache.contains(resname)) {
             return null;
+        }
 
-        if (templateCache.containsKey(resname))
+        if (templateCache.containsKey(resname)) {
             return templateCache.get(resname);
+        }
 
         // now check if we have match defined in the config
         ConfigMarker marker = matches.get(resname);
         if (marker == null) {
             // try to match resource name with known patterns
-            for (ConfigMarker cm : patterns)
+            for (ConfigMarker cm : patterns) {
                 if (resname.matches(cm.match)) {
                     marker = cm;
                     break;
                 }
+            }
         }
 
         if (marker != null) {
@@ -77,10 +89,11 @@ public class MarkerFactory {
 
         for (ConfigGroup group : config.getGroups()) {
             for (ConfigMarker marker : group.markers) {
-                if (marker.ispattern)
+                if (marker.ispattern) {
                     patterns.add(marker);
-                else
+                } else {
                     matches.put(marker.match, marker);
+                }
             }
         }
     }
