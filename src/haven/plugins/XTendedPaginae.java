3a5b8b3b17d9a126e8db89be76c668e52040fd37
Simon Donne <donnesimon@gmail.com>
2016-03-13 15:36:13 +0100

    WIP on plugins

--- src/haven/plugins/XTendedPaginae.java
+++ src/haven/plugins/XTendedPaginae.java
@@ -7,53 +7,62 @@ import haven.Resource;
 import haven.TimerPanel;
 import haven.UI;
 import haven.WikiBrowser;
+import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class XTendedPaginae {
-
-    interface PluginCallable {
+    
+    public interface Plugin {
 
         void execute(UI ui);
     }
 
-    static Map<String, PluginCallable> dictionary = new HashMap<>();
+    static Map<String, Plugin> dictionary = new HashMap<>();
 
     public static void loadXTendedPaginae(UI ui) {
         loadBaseXTendedPaginae(ui);
+        loadPluginXTendedPaginae(ui);
     }
 
-    public static void loadBaseXTendedPaginae(UI ui) {
+    static void loadPluginXTendedPaginae(UI ui)
+    {
+        File plugin_folder = new File(Config.pluginfolder);
+        String[] plugin_names = plugin_folder.list();
+        
+    }
+    
+    static void loadBaseXTendedPaginae(UI ui) {
         Glob glob = ui.sess.glob;
         Collection<Glob.Pagina> p = glob.paginae;
         p.add(glob.paginafor(Resource.load("paginae/act/add")));
         p.add(glob.paginafor(Resource.load("paginae/add/timer")));
-        dictionary.put("timers", new PluginCallable() {
+        dictionary.put("timers", new Plugin() {
             public void execute(UI ui) {
                 TimerPanel.toggle();
             }
         });
         p.add(glob.paginafor(Resource.load("paginae/add/wiki")));
-        dictionary.put("wiki", new PluginCallable() {
+        dictionary.put("wiki", new Plugin() {
             public void execute(UI ui) {
                 WikiBrowser.toggle();
             }
         });
         p.add(glob.paginafor(Resource.load("paginae/add/craft")));
-        dictionary.put("craft", new PluginCallable() {
+        dictionary.put("craft", new Plugin() {
             public void execute(UI ui) {
                 ui.gui.toggleCraftWnd();
             }
         });
         p.add(glob.paginafor(Resource.load("paginae/add/radius")));
-        dictionary.put("radius", new PluginCallable() {
+        dictionary.put("radius", new Plugin() {
             public void execute(UI ui) {
                 Config.toggleRadius();
             }
         });
         p.add(glob.paginafor(Resource.load("paginae/add/hotkey")));
-        dictionary.put("hotkey", new PluginCallable() {
+        dictionary.put("hotkey", new Plugin() {
             public void execute(UI ui) {
                 HotkeyListWindow.instance(ui).toggle();
             }
