23d77d67aae516cd4a7c77e6b2b29124845a3417
VEiDER <veider@ginaz.darknet>
2020-02-22 22:38:18 -0600

    regenerate ver

--- update/gen_ver.sh
+++ update/gen_ver.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+
+# Grab build results
 cp ../build/vsalem.jar .
 cp ../build/lclient-res.jar .
+
+# Calculate all checksums
 for i in $(find . -type f -exec echo {} \; | cut -c3- |grep -v ver); do echo "$i=$(sha256sum $i|cut -d' ' -f 1)"; done > ver.tmp
+
+# Include Salem redistributables
+echo "builtin-res.jar=$(sha256sum ../build/builtin-res.jar |cut -d' ' -f 1)" >>ver.tmp
+echo "salem-res.jar=$(sha256sum ../build/salem-res.jar |cut -d' ' -f 1)" >>ver.tmp
+
+# Finally calculate checksum of checksums
 sha256sum ver.tmp|cut -d' ' -f 1 >ver
 cat ver.tmp >>ver
-echo "builtin-res.jar=$(sha256sum ../build/builtin-res.jar |cut -d' ' -f 1)" >>ver
-echo "salem-res.jar=$(sha256sum ../build/salem-res.jar |cut -d' ' -f 1)" >>ver
 
