69de902218162df792863508d6979cb38366ac4f
VEiDER <veider@ginaz.darknet>
2020-02-22 22:31:42 -0600

    include salem resource files to update

--- update/gen_ver.sh
+++ update/gen_ver.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+cp ../build/vsalem.jar .
+cp ../build/lclient-res.jar .
 for i in $(find . -type f -exec echo {} \; | cut -c3- |grep -v ver); do echo "$i=$(sha256sum $i|cut -d' ' -f 1)"; done > ver.tmp
 sha256sum ver.tmp|cut -d' ' -f 1 >ver
 cat ver.tmp >>ver
+echo "builtin-res.jar=$(sha256sum ../build/builtin-res.jar |cut -d' ' -f 1)" >>ver
+echo "salem-res.jar=$(sha256sum ../build/salem-res.jar |cut -d' ' -f 1)" >>ver
 
