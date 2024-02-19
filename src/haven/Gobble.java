14fd214dd8600e8be618c6700dfaf5e550f4e3eb
Simon Donné <donnesimon@gmail.com>
2024-02-19 19:47:00 +0100

    Revised the gobble system to change how food groups add up toward a new total
    that limits the qty able to be gobbled in a session.

--- src/haven/Gobble.java
+++ src/haven/Gobble.java
@@ -81,6 +81,7 @@ public class Gobble extends SIWidget {
 	public void tick(double dt) {
 	    if(updt) {
 		nw = 0;
+		double cm = 0;
 		int aw = 0;
 		for(TypeMod m : mods) {
 		    if(m.rn == null) {
@@ -100,7 +101,12 @@ public class Gobble extends SIWidget {
 			}
 		    }
 		    if(m.ra == null) {
-			Text rt = tnf.render((int)Math.round(m.a * 100) + "%", new Color(255, (int)(255 * m.a), (int)(255 * m.a)));
+			if((m.a * 100) > 10) {
+			    cm = 10;
+			} else {
+			    cm = m.a * 100;
+			}			    
+			Text rt = tnf.render((int)Math.round(m.a * 100) + " ", new Color(255, (int)(255 - (25.5 * (cm - 1))), (int)(255 - (25.5 * (cm - 1)))));
 			m.ra = new TexI(rasterimg(blurmask2(rt.img.getRaster(), 2, 1, new Color(0, 0, 0))));
 		    }
 		    nw = Math.max(nw, m.rn.sz().x);
@@ -188,14 +194,14 @@ public class Gobble extends SIWidget {
     }
 
     private WritableRaster rgmeter(GobbleInfo food, double e, int t) {
-	return(alphablit(rmeter(hibars[t].getRaster(), lev[t] + (int)(e * food.h[t]), max),
-			 rmeter(lobars[t].getRaster(), lev[t] + (int)(e * food.l[t]), max),
+	return(alphablit(rmeter(hibars[t].getRaster(), lev[t] + (int)(food.h[t]), max),
+			 rmeter(lobars[t].getRaster(), lev[t] + (int)(food.l[t]), max),
 			 Coord.z));
     }
 
     private WritableRaster lgmeter(GobbleInfo food, double e, int t) {
-	return(alphablit(lmeter(hibars[t].getRaster(), lev[t] + (int)(e * food.h[t]), max),
-			 lmeter(lobars[t].getRaster(), lev[t] + (int)(e * food.l[t]), max),
+	return(alphablit(lmeter(hibars[t].getRaster(), lev[t] + (int)(food.h[t]), max),
+			 lmeter(lobars[t].getRaster(), lev[t] + (int)(food.l[t]), max),
 			 Coord.z));
     }
 
